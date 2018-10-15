fc061c97da87f66cf6118e957b954eae2e79b19e
Mureed Sultan <sultan.0313@gmail.com>
2018-10-16 01:50:36 +0500

    Added reverse proxy

--- express-docker/app.js
+++ express-docker/app.js
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use('/', indexRouter);
-app.use('/news', express.static(path.join(__dirname, 'public')));
 app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
