a536b2daa4b82f0eaed08546f1994bd18cce5393
Mureed Sultan <sultan.0313@gmail.com>
2019-05-13 11:47:45 -0400

    Kubernetes working
    Moving from docker-compose to kubernetes complete

--- express-docker/routes/index.js
+++ express-docker/routes/index.js
@@ -1,73 +1,78 @@
-const express = require('express');
-const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+const path = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', {
-		title: 'Home'
-	});
-});
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  })
+})
 
-router.get('/telemedicine', function(req, res, next) {
-	res.render('telemedicine', {
-		title: 'Telemedicine'
-	});
-});
+router.get('/telemedicine', function (req, res, next) {
+  res.render('telemedicine', {
+    title: 'Telemedicine'
+  })
+})
 
-router.get('/specialties', function(req, res, next) {
-	res.render('specialties', {
-		title: 'Specialties'
-	});
-});
+router.get('/specialties', function (req, res, next) {
+  res.render('specialties', {
+    title: 'Specialties'
+  })
+})
 
-router.get('/patients', function(req, res, next) {
-	res.render('patients', {
-		title: 'Patients'
-	});
-});
+router.get('/patients', function (req, res, next) {
+  res.render('patients', {
+    title: 'Patients'
+  })
+})
 
-router.get('/partners', function(req, res, next) {
-	let partnersFilter = [];
-	let clientsFilter = [];
-	const partners = fs.readdirSync('./public/dist/img/partners/');
-	const clients = fs.readdirSync('./public/dist/img/clients/');
+router.get('/partners', function (req, res, next) {
+  let partnersFilter = []
+  let clientsFilter = []
+  const partners = fs.readdirSync('./public/dist/img/partners/')
+  const clients = fs.readdirSync('./public/dist/img/clients/')
 
-	for (var i = 0; i < partners.length; i++) {
-		if (path.extname(partners[i]) == ".png" || path.extname(partners[i]) == ".jpg") {
-			console.log(partners[i]);
-			partnersFilter.push(partners[i]);
-		}
-	}
-	for (var i = 0; i < clients.length; i++) {
-		if (path.extname(clients[i]) == ".png" || path.extname(clients[i]) == ".jpg") {
-			console.log(clients[i]);
-			clientsFilter.push(clients[i]);
-		}
-	}
-	res.render('partners', {
-		title: 'Partners',
-		partners: partnersFilter,
-		clients: clientsFilter
-	});
+  for (var i = 0; i < partners.length; i++) {
+    if (
+      path.extname(partners[i]) == '.png' ||
+      path.extname(partners[i]) == '.jpg'
+    ) {
+      console.log(partners[i])
+      partnersFilter.push(partners[i])
+    }
+  }
+  for (var i = 0; i < clients.length; i++) {
+    if (
+      path.extname(clients[i]) === '.png' ||
+      path.extname(clients[i]) === '.jpg'
+    ) {
+      console.log(clients[i])
+      clientsFilter.push(clients[i])
+    }
+  }
+  res.render('partners', {
+    title: 'Partners',
+    partners: partnersFilter,
+    clients: clientsFilter
+  })
+})
 
-});
+router.get('/faq', function (req, res, next) {
+  res.render('faq', {
+    title: 'FAQ'
+  })
+})
 
-router.get('/faq', function(req, res, next) {
-	res.render('faq', {
-		title: 'FAQ'
-	});
-});
+router.get('/contact-us', function (req, res, next) {
+  res.render('contact-us', {
+    title: 'Contact Us'
+  })
+})
 
-router.get('/contact-us', function(req, res, next) {
-	res.render('contact-us', {
-		title: 'Contact Us'
-	});
-});
+router.post('/contact-us', function (req, res) {
+  res.send('1')
+})
 
-router.post('/contact-us', function(req, res) {
-	res.send('1');
-});
-
-module.exports = router;
+module.exports = router
--- express-docker/routes/news.js
+++ express-docker/routes/news.js
@@ -1,101 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
-const chalk = require('chalk');
-const log = console.log;
+const express = require('express')
+const router = express.Router()
+const chalk = require('chalk')
+const log = console.log
 
-const white = chalk.whiteBright.bold;
-const red = chalk.redBright.bold;
-const green = chalk.greenBright.bold;
-const cyan = chalk.cyanBright.bold;
+const red = chalk.redBright.bold
+const green = chalk.greenBright.bold
 
-const client_id = 'ghost-frontend';
-const client_secret = 'fff48a18b688';
+router.get('/', function (req, res, next) {
+  if (process.env.NEWS_URL) {
+    log(green('\nNODE_ENV is set\n'))
+    res.redirect(process.env.NEWS_URL)
+  } else {
+    log(red('\nPlease set NODE_ENV\n'))
+    res.render('error', {
+      title: 'Error',
+      error: {
+        status: '500',
+        message: 'NEWS_URL not set',
+        stack: 'Please set NEWS_URL in env vars'
+      }
+    })
+  }
+})
 
-if (process.env.NODE_ENV == "production") {
-    api = 'http://ghost:2368/ghost/api/v0.1/posts/';
-    log(white('\nEnvironment: Production\n'));
-} else if (process.env.NODE_ENV == "development") {
-    api = 'http://localhost:2368/ghost/api/v0.1/posts/';
-    log(white('\nEnvironment: Development\n'));
-} else {
-    log(red('\nPlease set NODE_ENV\n'));
-}
-
-router.get('/', function(req, res, next) {
-    log(cyan('\n========== NEWS ==========\n'));
-    axios.get(api, {
-            params: {
-                client_id,
-                client_secret
-            }
-        })
-        .then(function(response) {
-            res.render('news/news', {
-                title: 'News',
-                posts: response.data.posts
-            });
-            log(green(`\nSuccess: ${api}\n`));
-        })
-        .catch(function(error) {
-            let err;
-            if (typeof error.response == "undefined") {
-                err = {
-                    status: '500',
-                    statusText: "API can't be reached",
-                    stack: error
-                }
-            } else {
-                err = {
-                    status: error.response.status,
-                    message: error.response.statusText,
-                    stack: 'Axios ' + error
-                }
-            }
-            res.render('error', {
-                title: 'Error',
-                error: {
-                    status: err.status,
-                    message: err.statusText,
-                    stack: err.stack
-                }
-            });
-            //log(error);
-            log(red(`\nError: ${api}\n`));
-        });
-});
-
-router.get('/:article', function(req, res, next) {
-    log(cyan('\n========== ARTICLE =========='));
-    const article = req.params.article;
-    let url = api + 'slug/' + article;
-    //*
-    axios.get(url, {
-            params: {
-                client_id,
-                client_secret
-            }
-        })
-        .then(function(response) {
-            res.render('news/news-single', {
-                title: 'News',
-                posts: response.data.posts
-            });
-            log(green(`\nSuccess: ${article}\n`));
-        })
-        .catch(function(error) {
-            res.render('error', {
-                title: 'Error',
-                error: {
-                    status: error.response.status,
-                    message: error.response.statusText,
-                    stack: 'Axios ' + error
-                }
-            });
-            //log(error);
-            log(red(`\nError: ${article}\n`));
-        });
-    //*/
-});
-
-module.exports = router;
+module.exports = router
